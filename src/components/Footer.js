8689902bef68faa2098b7f0e43b9f1285523cd2a
Gaetano <gaetano.gagliano@unikore.it>
2025-03-11 17:32:02 +0100

    refined2(altre modifiche mancanti)

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="footer-text">© 2025 Nome del Progetto</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
